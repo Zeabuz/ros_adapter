f907d7ae36d38fe1fb0b70a71ab5837bc11f2180
Thomas Skarshaug <thomskar@ntnu.no>
2020-11-07 09:29:18 -0800

    Add cat streaming

--- scripts/streamer.py
+++ scripts/streamer.py
@@ -1,26 +1,44 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
 import rospy
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
+
+from cv_bridge import CvBridge, CvBridgeError
 
 import grpc
+import cv2
 
 import sensordata_pb2
 import sensordata_pb2_grpc
 
+import numpy as np
+
 def streamer():
+
+    # Setting up cv_bridge
+    bridge = CvBridge()
 
     # Setting up sensordata channel
     sensordata_channel = grpc.insecure_channel('localhost:50083')
     sensordata_stub = sensordata_pb2_grpc.SensordataStub(sensordata_channel)
 
-    pub = rospy.Publisher('image', String, queue_size=10)
+    
+    cv_image = cv2.imread('/home/thomas/dev/catkin_ws/src/ros_adapter/scripts/cat.jpg')
+    cv_image = cv_image.reshape(549, 976, 3)
+    cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2BGR)
+    msg = 0 
+    try: 
+        msg = bridge.cv2_to_imgmsg(cv_image, 'bgr8')
+    except CvBridgeError as e:
+        print(e)
+
+
+    pub = rospy.Publisher('image', Image, queue_size=10)
     rospy.init_node('streamer', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(1) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world " + str(rospy.get_time())
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        pub.publish(msg)
         rate.sleep()
 
 if __name__ == '__main__':
