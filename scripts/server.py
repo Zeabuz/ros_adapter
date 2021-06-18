74d05424175c0de84638855563d1bb114e2b7152
Thomas Skarshaug <thomas.skarshaug@zeabuz.com>
2021-06-18 10:12:05 +0200

    Some cleanup and parameter renaming

--- scripts/server.py
+++ scripts/server.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python2
 
+import sys
+import yaml
+
 from concurrent import futures
 
 import rospy
 import roslib
+import rosparam
 from rosgraph_msgs.msg import Clock
 
 import geometry_msgs.msg as geomsgs
@@ -20,6 +24,8 @@ from cv_bridge import CvBridge, CvBridgeError
 
 import grpc
 import cv2
+
+from config import parser
 
 from sensor_streaming import sensor_streaming_pb2
 from sensor_streaming import sensor_streaming_pb2_grpc
@@ -68,7 +74,7 @@ class SensorStreaming(sensor_streaming_pb2_grpc.SensorStreamingServicer):
         except CvBridgeError as e:
             print(e)
 
-        camera_pubs[request.frame_id].publish(msg)
+        camera_pubs[request.frame_id.encode("ascii", 'ignore')].publish(msg)
 
         return sensor_streaming_pb2.CameraStreamingResponse(success=True)
 
@@ -123,7 +129,7 @@ class SensorStreaming(sensor_streaming_pb2_grpc.SensorStreamingServicer):
         all the data needed to create and publish a RadarSpoke
         ROS message.
         """
-        
+
         number_of_spokes = request.numSpokes
 
         for i in range(number_of_spokes):
@@ -135,7 +141,9 @@ class SensorStreaming(sensor_streaming_pb2_grpc.SensorStreamingServicer):
             header.frame_id = "milliampere_radar"
             header.stamp = rospy.Time.from_sec(request.timeInSeconds[i])
             radar_spoke_msg.azimuth = request.azimuth[i]
-            radar_spoke_msg.intensity = request.radarSpokes[i * request.numSamples : i * request.numSamples + request.numSamples]
+            radar_spoke_msg.intensity = request.radarSpokes[
+                    i * request.numSamples:
+                    i * request.numSamples + request.numSamples]
 
             radar_spoke_msg.range_start = request.rangeStart
             radar_spoke_msg.range_increment = request.rangeIncrement
@@ -161,13 +169,13 @@ class Navigation(navigation_pb2_grpc.NavigationServicer):
 
         # TODO: This frame_id should be dynamically set from a config file.
         nav_header = std_msgs.msg.Header(
-            frame_id=simulation_params["geo_frame"],
+            frame_id=simulation_params["scenario" + scenario_id]["geo_frame"],
             stamp=rospy.Time.from_sec(request.timeStamp)
         )
 
         transform = geomsgs.TransformStamped()
         transform.header = nav_header
-        
+
         position = geomsgs.Point()
         position.x = request.position.x
         position.y = request.position.y
@@ -187,22 +195,19 @@ class Navigation(navigation_pb2_grpc.NavigationServicer):
             )
         )
 
-        #pdb.set_trace()
-
-        transform.transform=geomsgs.Transform(
+        transform.transform = geomsgs.Transform(
                 translation=position,
                 rotation=orientation
                 )
 
         vessel_name = request.vesselName
-        
+
         if vessel_name == "ego":
             ego_pose_pub.publish(pose_msg)
             transform.child_frame_id = "vessel_center"
         else:
             transform.child_frame_id = simulation_params["target_vessel_center_names"][vessel_name]
             target_pose_pubs[request.vesselName].publish(pose_msg)
-
 
         tf_pub.sendTransform(transform)
 
@@ -216,7 +221,6 @@ class Navigation(navigation_pb2_grpc.NavigationServicer):
         angular_vel.y = request.angularVelocity.y
         angular_vel.z = request.angularVelocity.z
 
-    
         twist_msg = geomsgs.TwistStamped(
             header=nav_header,
             twist=geomsgs.Twist(
@@ -232,7 +236,7 @@ class Navigation(navigation_pb2_grpc.NavigationServicer):
 
 def serve(server_ip, server_port, camera_pubs,
           lidar_pub, radar_pub, clock_pub,
-          ego_pose_pub, target_pose_pubs, 
+          ego_pose_pub, target_pose_pubs,
           ego_twist_pub, target_twist_pubs, tf_pub):
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
@@ -242,7 +246,8 @@ def serve(server_ip, server_port, camera_pubs,
             server)
 
     navigation_pb2_grpc.add_NavigationServicer_to_server(
-        Navigation(ego_pose_pub, target_pose_pubs, ego_twist_pub, target_twist_pubs, tf_pub),
+        Navigation(ego_pose_pub, target_pose_pubs,
+                   ego_twist_pub, target_twist_pubs, tf_pub),
         server)
 
     server.add_insecure_port(server_ip + ':' + str(server_port))
@@ -254,20 +259,26 @@ def serve(server_ip, server_port, camera_pubs,
 if __name__ == '__main__':
 
     rospy.init_node('syntetic_data', anonymous=True)
-    simulation_params = rospy.get_param('~')
+
+    config_dir_path = sys.argv[1]
+    scenario_id = sys.argv[2]
+
+    simulation_params = parser.parse_scenario_config(config_dir_path, scenario_id)
+
     server_ip = simulation_params["server_ip"]
     server_port = simulation_params["server_port"]
 
-    ego_vessel_name = simulation_params["ego_vessel_name"]
+    ego_vessel_name = simulation_params["scenario" + scenario_id]["ego_vessel_name"]
     target_vessel_names = simulation_params["target_vessel_names"]
 
     cam_ids = simulation_params['ego_optical_camera_frame_ids']
     camera_pubs = dict()
     for cam_id in cam_ids:
-        camera_pubs[cam_id] = rospy.Publisher(simulation_params['ego_optical_camera_prefix'] + 
-                                              cam_id + 
-                                              simulation_params['ego_optical_camera_postfix'],
-                                              Image, queue_size=10)
+        camera_pubs[cam_id] = rospy.Publisher(
+                simulation_params['ego_optical_camera_prefix'] +
+                cam_id +
+                simulation_params['ego_optical_camera_postfix'],
+                Image, queue_size=10)
 
     lidar_pub = rospy.Publisher(simulation_params['ego_lidar_topic'],
                                 PointCloud2,
@@ -279,16 +290,21 @@ if __name__ == '__main__':
 
     clock_pub = rospy.Publisher('clock', Clock, queue_size=10)
 
-    ego_pose_pub = rospy.Publisher(simulation_params['ego_pose_topic'], geomsgs.PoseStamped, queue_size=10)
-    ego_twist_pub = rospy.Publisher(simulation_params['ego_twist_topic'], geomsgs.TwistStamped, queue_size=10)
-    
+    ego_pose_pub = rospy.Publisher(
+            simulation_params['ego_pose_topic'],
+            geomsgs.PoseStamped, queue_size=10)
+
+    ego_twist_pub = rospy.Publisher(
+            simulation_params['ego_twist_topic'],
+            geomsgs.TwistStamped, queue_size=10)
+
     target_pose_pubs = dict()
     for target_name in target_vessel_names:
         target_pose_pubs[target_name] = rospy.Publisher(
                 simulation_params['target_vessel_pose_topics'][target_name],
                 geomsgs.PoseStamped,
                 queue_size=10)
-    
+
     target_twist_pubs = dict()
     for target_name in target_vessel_names:
         target_twist_pubs[target_name] = rospy.Publisher(
@@ -300,5 +316,5 @@ if __name__ == '__main__':
 
     serve(server_ip, server_port, camera_pubs,
           lidar_pub, radar_pub, clock_pub,
-          ego_pose_pub, target_pose_pubs, 
+          ego_pose_pub, target_pose_pubs,
           ego_twist_pub, target_twist_pubs, tf_pub)
